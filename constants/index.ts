502393ec738d3f8ab8ddba5ab2562dd8bcd17ef4
difadlyaulhaq <difadlyaulhaq2@gmail.com>
2025-09-03 17:48:47 +0700

    fix(UI): Correctly display icons for upcoming and previous sidebar links
    The icons for the "Upcoming" and "Previous" navigation links were missing due to a potential issue with the SVG files or paths. This has been resolved, and all icons now appear as expected.

--- constants/index.ts
+++ constants/index.ts
@@ -2,26 +2,35 @@ export const sidebarLinks = [
   {
     imgURL: '/icons/Home.svg',
     route: '/',
-    label: 'Home'
+    label: 'Home',
+  },
+
+  {
+    imgURL: '/icons/upcoming.svg',
+    route: '/upcoming',
+    label: 'Upcoming',
   },
   {
-    imgURL: '/icons/Upcoming.svg',
-    route: '/upcoming',
-    label: 'Upcoming'
-  },
-    {
-    imgURL: '/icons/Previous.svg',
+    imgURL: '/icons/previous.svg',
     route: '/previous',
-    label: 'Previous'
+    label: 'Previous',
   },
-    {
+  {
     imgURL: '/icons/Video.svg',
     route: '/recordings',
-    label: 'Recordings'
+    label: 'Recordings',
   },
-    {
+  {
     imgURL: '/icons/add-personal.svg',
     route: '/personal-room',
-    label: 'Personal Room'
-  }
+    label: 'Personal Room',
+  },
+];
+
+export const avatarImages = [
+  '/images/avatar-1.jpeg',
+  '/images/avatar-2.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png',
 ];
